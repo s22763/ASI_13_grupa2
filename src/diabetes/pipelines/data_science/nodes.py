0c9a41630cbde45c9fa933a38e7f82c0828afb06
Jakub Guliński <gulinskijakub@gmail.com>
2023-11-06 22:39:53 +0100

    Add wandb metrics

--- src/diabetes/pipelines/data_science/nodes.py
+++ src/diabetes/pipelines/data_science/nodes.py
@@ -2,16 +2,25 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, mean_squared_error
 import pickle
+import wandb
 
 def create_model(X_train:pd.DataFrame, y_train:pd.DataFrame):
     clf = RandomForestClassifier(n_estimators=100, random_state=42)
     clf.fit(X_train, y_train)
+
+    wandb.sklearn.plot_learning_curve(clf, X_train, y_train)
     return clf
 
 def predict(X_validate:pd.DataFrame, y_validate:pd.DataFrame, clf:RandomForestClassifier):
     y_pred = clf.predict(X_validate)
+    
+    wandb.sklearn.plot_confusion_matrix(y_validate, y_pred)
+
+    
     accuracy = accuracy_score(y_validate, y_pred)
+    wandb.log({"accuracy": accuracy})
     return accuracy
     
 def save_model(clf:RandomForestClassifier):
     pickle.dump(clf, open('model.pkl', 'wb'))
+    
